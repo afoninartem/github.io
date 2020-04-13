96de4c2adf5b0807ddcaaf32f749d7a6ee5d8dcc
afoninartem <47443887+afoninartem@users.noreply.github.com>
2020-04-13 16:10:13 +0300

    Burger-menu-adaptive

--- js/main.js
+++ js/main.js
@@ -8,3 +8,17 @@
         }
     };
 }());
+
+//BURGER HANDLER 
+
+(function(){
+    const burgerItem = document.querySelector('.burger');
+    const exit = document.querySelector('.header__nav-close');
+    const menu = document.querySelector('.header__nav');
+    burgerItem.addEventListener('click', () => {
+        menu.classList.add('header__nav-active');
+    });
+    exit.addEventListener('click', () => {
+        menu.classList.remove('header__nav-active');
+    })
+}());
